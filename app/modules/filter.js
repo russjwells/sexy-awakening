ddcd15ae9f7cb3cdedc1d20e38f07c45dbe6eb6b
Russ Wells <russ@russdigital.com>
2018-04-10 12:35:53 -0700

    gender filtering

--- app/modules/filter.js
+++ app/modules/filter.js
@@ -3,5 +3,15 @@ import _ from 'lodash'
 export default (profiles, user) => {
     const rejectMe = _.reject(profiles, profile => profile.uid === user.uid)
 
-    return rejectMe
+    const filterGender = _.filter(rejectMe, profile => {
+        const userShowMen = user.showMen && profile.gender === 'male'
+        const userShowWomen = user.showWomen && profile.gender === 'female'
+
+        const profileShowMen = profile.showMen && user.gender === 'male'
+        const profileShowWomen = profile.showWomen && user.gender === 'female'
+
+        return (userShowMen || userShowWomen) && (profileShowMen || profileShowWomen)
+    })
+
+    return filterGender
 }
