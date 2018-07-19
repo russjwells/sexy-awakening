1f516b8746324af9b845a18bce914017191dd312
Russ Wells <russ@russdigital.com>
2018-07-19 11:19:18 -0700

    fixed issue with nav between the chat and profile view

--- app/screens/viewProfile.js
+++ app/screens/viewProfile.js
@@ -19,36 +19,15 @@ export default class ViewProfile extends Component {
 
     state = {
         user: this.props.navigation.state.params.user,
-        ageRangeValues: this.props.navigation.state.params.ageRangeValues,
-        distanceValue: this.props.navigation.state.params.distanceValue,
-        showMen: this.props.navigation.state.params.showMen,
-        showWomen: this.props.navigation.state.params.showWomen,
-        bio: this.props.navigation.state.params.profile.bio,
-    }
-
-    updateUser = (key, value) => {
-        const {uid} = this.state.user
-        firebase.database().ref('users').child(uid)
-        .update({[key]:value})
-    }
-
-    save = (bio) => {
-        //updateUser
-        this.updateUser('bio', bio)
-        //go home
-        this.props.navigation.navigate('Home', {user: this.props.navigation.state.params.user})
     }
     
     render() {
         const {width, height} = Dimensions.get('window')
-        const {first_name, work, id} = this.props.navigation.state.params.profile
-        //const {ageRangeValues, distanceValue, showMen, showWomen} = this.state
-        //const bio = (work && work[0] && work[0].position) ? work[0].position.name : null
-        let bio = this.state.bio
+        const {first_name, bio, id} = this.props.navigation.state.params.profile
         return(
             <KeyboardAvoidingView style={styles.container} behavior={'padding'} keyboardVerticalOffset={60}>
                 <View style={styles.navbar}>
-                    <TouchableHighlight style={styles.navback} onPress={() => this.props.navigation.navigate('Home', {user: this.props.navigation.state.params.user})}>
+                    <TouchableHighlight style={styles.navback} onPress={() => this.props.navigation.navigate('Chat', {user: this.props.navigation.state.params.user, profile: this.props.navigation.state.params.profile})}>
                         <View style={styles.navback}>
                             <Text><Feather name="arrow-left" size={32} color="black" /></Text>
                         </View>
